5d10291f9e22ba78cbd83ed1edd38e712b21e468
Pedro Sousa <83181309+pedrossdemelo@users.noreply.github.com>
2022-06-12 19:10:47 -0300

    solve: Merge k Sorted Lists

---
+++ leetcode/Merge k Sorted Lists.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/merge-k-sorted-lists/
+
+from idna import valid_contextj
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+# n = n of linked lists
+# m = average amount of nodes in linked lists
+# Time: O(n * m) | Space: O(n * m)
+def mergeKLists(lists):
+    values = []
+    for head in lists:
+        while head:
+            values.append(head.val)
+            head = head.next
+    values.sort()
+    if not values:
+        return []
+    head = ListNode(values.pop())
+    while values:
+        head = ListNode(values.pop(), head)
+    return head
