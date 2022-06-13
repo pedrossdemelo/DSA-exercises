494d9c5ff5e340e9e13bf680a38ab607ec43d031
Pedro Sousa <83181309+pedrossdemelo@users.noreply.github.com>
2022-06-13 13:10:10 -0300

    solve: Subsets

---
+++ leetcode/Subsets.py
@@ -0,0 +1,28 @@
+# https://leetcode.com/problems/subsets/
+
+
+# Time: O(2^n) | Space: O(n)
+def subsets(nums):
+    result, picks = [], []
+
+    def dfs(i):
+        if i == len(nums):
+            return result.append(picks.copy())
+        dfs(i + 1)
+        picks.append(nums[i])
+        dfs(i + 1)
+        picks.pop()
+
+    dfs(0)
+    return result
+
+
+# Time: O(n^2) | Space: O(n)
+def subsets(nums):
+    results = [[]]
+    for num in nums:
+        results.extend([res + [num] for res in results])
+    return results
+
+
+print(subsets([1, 2, 3]))
