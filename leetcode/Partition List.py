fbb4aa28143f403c5d21fc7cdc4588e063ec7b66
Pedro Sousa <83181309+pedrossdemelo@users.noreply.github.com>
2022-06-18 20:46:59 -0300

    solve: Partition List

---
+++ leetcode/Partition List.py
@@ -0,0 +1,31 @@
+# https://leetcode.com/problems/partition-list/
+
+class ListNode:
+    def __init__(self, val=0, next=None) -> None:
+        self.val = val
+        self.next = next
+
+    def __repr__(self) -> str:
+        return f"{self.val} -> {self.next}"
+
+def partition(head, x):
+    gTEx_sentinel = gTEx_tail = ListNode()
+    sentinel = ListNode(next=head)
+    curr, prev = head, sentinel
+    while curr:
+        if curr.val >= x:
+            prev.next = curr.next
+            curr.next = None
+            gTEx_tail.next = curr
+            gTEx_tail = curr
+            curr = prev.next
+        else:
+            prev = prev.next
+            curr = curr.next
+
+    prev.next = gTEx_sentinel.next
+    return sentinel.next
+
+list = ListNode(1, ListNode(4, ListNode(3, ListNode(2, ListNode(5, ListNode(2))))))
+
+print(partition(list, 3))
