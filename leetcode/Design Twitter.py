c3ef104ac9a9e7db10a39122ced5ef12181bc82c
Pedro Sousa <83181309+pedrossdemelo@users.noreply.github.com>
2022-06-26 21:12:49 -0300

    improve: time

--- leetcode/Design Twitter.py
+++ leetcode/Design Twitter.py
@@ -1,6 +1,7 @@
 # https://leetcode.com/problems/design-twitter/
 
 from collections import defaultdict, deque
+import heapq
 from typing import List
 
 
@@ -13,13 +14,22 @@ class Twitter:
     # O(1)
     def postTweet(self, userId: int, tweetId: int) -> None:
         self.tcount += 1
-        self.tweets[userId].appendleft((tweetId, self.tcount))
+        self.tweets[userId].appendleft((self.tcount, tweetId))
 
-    # O(nmlognm)
+    # n = following
+    # O(nlog10)
     def getNewsFeed(self, userId: int) -> List[int]:
-        feed = [*self.tweets[userId]] + [t for user in self.following[userId] for t in self.tweets[user]]
-        feed.sort(key=lambda x: x[1], reverse=True)
-        return [t[0] for t in feed[:10]]
+        heap = []
+        for user in self.following[userId] | {userId}:
+            for time, tweetid in self.tweets[user]:
+                if len(heap) < 10:
+                    heapq.heappush(heap, (time, tweetid))
+                elif time > heap[0][0]:
+                    heapq.heapreplace(heap, (time, tweetid))
+
+        heap.sort(reverse=True)
+
+        return [tweetid for _, tweetid in heap]
 
     # O(1)
     def follow(self, followerId: int, followeeId: int) -> None:
@@ -40,8 +50,10 @@ methods = [
     "getNewsFeed",
 ]
 args = [[1, 5], [1], [1, 2], [2, 6], [1], [1, 2], [1]]
+expected = [None, [5], None, None, [6, 5], None, [5]]
 
 t = Twitter()
 
-for method, arg in zip(methods, args):
-    print(getattr(t, method)(*arg))
+for method, arg, expect in zip(methods, args, expected):
+    print()
+    print(f"expected: {expect} | got: {getattr(t, method)(*arg)}")
