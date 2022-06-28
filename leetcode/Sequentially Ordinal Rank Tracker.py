ef4de3b5e5ed69cab224a8ae0857d523eecc397a
Pedro Sousa <83181309+pedrossdemelo@users.noreply.github.com>
2022-06-27 22:57:18 -0300

    solve: Sequentially Ordinal Rank Tracker

---
+++ leetcode/Sequentially Ordinal Rank Tracker.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/sequentially-ordinal-rank-tracker/
+
+class SORTracker:
+    def __init__(self):
+        
+
+    def add(self, name: str, score: int) -> None:
+        
+
+    def get(self) -> str:
