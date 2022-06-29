a275b02a13892c7e54276deffacbc039b30c5188
Pedro Sousa <83181309+pedrossdemelo@users.noreply.github.com>
2022-06-29 20:21:15 -0300

    solve: Heaters

---
+++ leetcode/Heaters.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/heaters/
+from typing import List
+
+
+def findRadius(houses: List[int], heaters: List[int]) -> int:
+    heaters.sort()
+    heater = 0
+    minradius = -float("inf")
+    for location in sorted(houses):
+        while heater < len(heaters) - 1 and abs(location - heaters[heater]) >= abs(
+            location - heaters[heater + 1]
+        ):
+            heater += 1
+        minradius = max(minradius, abs(location - heaters[heater]))
+    return minradius
+
+
+houses, heaters = ([1, 2, 3, 4, 5, 6, 9], [1, 4])
+print(findRadius(houses, heaters))
