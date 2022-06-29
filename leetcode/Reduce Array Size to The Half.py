decb4fedf05a368123a73f5d1665d995510433a9
Pedro Sousa <83181309+pedrossdemelo@users.noreply.github.com>
2022-06-28 22:04:32 -0300

    solve: Reduce Array Size to The Half

---
+++ leetcode/Reduce Array Size to The Half.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/reduce-array-size-to-the-half/
+
+from collections import Counter
+from heapq import heappop
+from math import ceil
+
+
+def minSetSize(arr):
+    n = len(arr)
+    removed = result = 0
+
+    for _, count in Counter(arr).most_common():
+        removed += count
+        result += 1
+        if removed >= n // 2:
+            break
+
+    return result
+
+
+arr = [3, 3, 3, 3, 5, 5, 5, 2, 2, 7]
+print(minSetSize(arr))
