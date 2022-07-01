10fb645c87f9a51a48dc48925cc8345722e870d4
Pedro Sousa <83181309+pedrossdemelo@users.noreply.github.com>
2022-07-01 01:55:56 -0300

    improve: space

--- leetcode/Trapping Rain Water.py
+++ leetcode/Trapping Rain Water.py
@@ -16,5 +16,26 @@ def trap(height):
             tallesti = r
     return sum(water - h for water, h in zip(trapped, height))
 
+
+# Time: O(n) | Space: O(1)
+def trap(h):
+    n, water = len(h), 0
+    left, right = 0, n-1
+    lmax, rmax = h[left], h[right]
+
+    while left < right:
+        if rmax < lmax:
+            right -= 1
+            hright = h[right]
+            rmax = max(rmax, hright)
+            water += max(min(rmax,lmax) - hright, 0)
+        else:
+            left += 1
+            hleft = h[left]
+            lmax = max(lmax, hleft)
+            water += max(min(rmax,lmax) - hleft, 0)
+
+    return water
+
 height = [0,1,0,2,1,0,1,3,2,1,2,1]
 print(trap(height))
