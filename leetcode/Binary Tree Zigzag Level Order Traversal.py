b757fdb12566548246d6c50d6689a790501498b5
Pedro Sousa <83181309+pedrossdemelo@users.noreply.github.com>
2022-07-02 19:53:21 -0300

    fix: link

--- leetcode/Binary Tree Zigzag Level Order Traversal.py
+++ leetcode/Binary Tree Zigzag Level Order Traversal.py
@@ -1,4 +1,5 @@
-# Definition for a binary tree node.
+# https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
