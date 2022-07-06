d8186b8fe762a92c0caae58fa9c43a85e915b18a
Pedro Sousa <83181309+pedrossdemelo@users.noreply.github.com>
2022-07-05 21:40:53 -0300

    solve: Max Area of Island

--- leetcode/Max Area of Island.py
+++ leetcode/Max Area of Island.py
@@ -1,7 +1,10 @@
 # https://leetcode.com/problems/max-area-of-island/
 
 # Time: O(n) | Space: O(n)
-def maxAreaOfIsland(grid):
+from collections import deque
+
+
+def maxAreaOfIsland(grid):  # DFS
     # 2 -> Visited territory
     # 1 -> Unvisited territory
     # 0 -> Water
@@ -15,13 +18,13 @@ def maxAreaOfIsland(grid):
         grid[row][col] = 2  # Mark as visited
         currisland += 1
 
-        paths = (
+        directions = (
             (row + 1, col),  # down
             (row - 1, col),  # up
             (row, col + 1),  # right
             (row, col - 1),  # left
         )
-        neighbours = [(r, c) for r, c in paths if 0 <= r < ROWS and 0 <= c < COLS]
+        neighbours = [(r, c) for r, c in directions if 0 <= r < ROWS and 0 <= c < COLS]
         for neighbour in neighbours:
             visit(*neighbour)
 
@@ -35,14 +38,50 @@ def maxAreaOfIsland(grid):
     return maxisland
 
 
-grid = [
-    [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
-    [0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0],
-    [0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
-]
+def maxAreaOfIsland(grid):  # BFS
+    # 2 -> Visited territory
+    # 1 -> Unvisited territory
+    # 0 -> Water
+    ROWS, COLS = len(grid), len(grid[0])
+    maxisland = 0
+
+    def bfs(row, col):
+        queue = deque([(row, col)])
+        currisland = 0
+
+        while queue:
+            row, col = queue.popleft()
+            if grid[row][col] != 1:
+                continue
+            grid[row][col] = 2
+            currisland += 1
+
+            directions = (
+                (row + 1, col),  # down
+                (row - 1, col),  # up
+                (row, col + 1),  # right
+                (row, col - 1),  # left
+            )
+
+            neighbours = [
+                (r, c)
+                for r, c in directions
+                if 0 <= r < ROWS and 0 <= c < COLS and grid[r][c] == 1
+            ]
+
+            for neighbour in neighbours:
+                queue.append(neighbour)
+
+        return currisland
+
+    for row in range(ROWS):
+        for col in range(COLS):
+            if grid[row][col] == 1:
+                maxisland = max(maxisland, bfs(row, col))
+
+    return maxisland
+
+
+grid = [[1, 1, 0, 0, 0], [1, 1, 0, 0, 0], [0, 0, 0, 1, 1], [0, 0, 0, 1, 1]]
+
 print(maxAreaOfIsland(grid))
