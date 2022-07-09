5993e0cb906945d4215862268c0f7cc182436419
Pedro Sousa <83181309+pedrossdemelo@users.noreply.github.com>
2022-07-09 18:57:01 -0300

    improve: space

--- leetcode/Island Perimeter.py
+++ leetcode/Island Perimeter.py
@@ -1,40 +1,17 @@
 # https://leetcode.com/problems/island-perimeter/
 
-
+# Time: O(n) | Space: O(1)
 def islandPerimeter(grid):
     ROWS, COLS = len(grid), len(grid[0])
-    # 0 = Water | 1 = Land | 2 = Explored Land
-    VISITED, result = 2, 0
-
-    def perimeter_and_neighbours(row, col):
-        DIRECTIONS = (
-            (row - 1, col),  # up
-            (row + 1, col),  # down
-            (row, col - 1),  # left
-            (row, col + 1),  # right
-        )
-        neighbours = [
-            (r, c)
-            for r, c in DIRECTIONS
-            if 0 <= r < ROWS and 0 <= c < COLS and grid[r][c] != 0
-        ]
-        perimeter = 4 - len(neighbours)
-        return perimeter, neighbours
-
-    def dfs(row, col):
-        nonlocal result
-        grid[row][col] = VISITED
-        perimeter, neighbours = perimeter_and_neighbours(row, col)
-        result += perimeter
-        for r, c in neighbours:
-            if grid[r][c] is not VISITED:
-                dfs(r, c)
+    result = 0
 
     for row in range(ROWS):
         for col in range(COLS):
             if grid[row][col] == 1:
-                dfs(row, col)
-                return result
+                result += 4
+                if row > 0 and grid[row-1][col]: result -= 2 # If territory up
+                if col > 0 and grid[row][col-1]: result -= 2 # If territory left
+    return result
 
 
 grid = [[0, 1, 0, 0], [1, 1, 1, 0], [0, 1, 0, 0], [1, 1, 0, 0]]
